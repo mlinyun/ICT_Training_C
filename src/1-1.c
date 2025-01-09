7a641fb87edce53249cc7ebcc4d32462b15a7d26
LinCanhui <lingyun2311@gmail.com>
2025-01-09 18:05:10 +0800

    C 语言简单剖析

---
+++ src/1-1.c
@@ -0,0 +1,7 @@
+#include <stdio.h> /** 编译预处命令*/
+
+// 主函数
+int main(void) {
+    printf("Hello ShiYanLou\n");
+    return 0;
+}
---
+++ src/1-2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("23 + 56 = %d\n", 23 + 56);
+    return 0;
+}
